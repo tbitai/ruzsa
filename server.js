63fa2b4609ef9c3cfbddc64731539f0448b14e26
Tamas Bitai <bitai.tamas@gmail.com>
2016-02-01 14:56:43 +0100

    Fix: host was not set in server.js

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ var helmet = require('helmet');
 var app = express();
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var host = process.env.OPENSHIFT_NODEJS_IP || 'localhost';
 
 // Middlewares, in order of execution
 app.use(helmet());
 app.use(express.static(__dirname));
 
-app.listen(port, function() {
-    console.log('ruzsa listening on port ' + port);
+app.listen(port, host, function() {
+    console.log('ruzsa listening on ' + host + ':' + port);
 });
