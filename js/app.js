b9cfa58d3639343566052b2b395696083c7b833c
Tamas Bitai <bitai.tamas@gmail.com>
2016-03-16 21:57:18 +0100

    addCandidates() + UI; fix: submit() shouldn't treat nodes w/o 'connectId' key as connected; fix: don't allow addLeaves()/addCandidates() if there are empty nodes

--- js/app.js
+++ js/app.js
@@ -4,8 +4,7 @@ angular.module('ruzsa', ['sf.treeRepeat', 'ngMaterial', 'ngMessages'])
             .primaryPalette('blue')
             .accentPalette('grey');
     })
-    .controller('treeController', function($scope){
-        
+    .controller('treeController', function($scope, $mdDialog){
         $scope.treeData = {
             formula: new WFF('Tet(a) ∧ Tet(b) ∨ Dodec(a) ∨ Tet(b)'),
             editable: false,
@@ -42,18 +41,23 @@ angular.module('ruzsa', ['sf.treeRepeat', 'ngMaterial', 'ngMessages'])
         };
         /*
         $scope.treeData = null; */
-        $scope.submit = function(node) {
+        $scope.setFormula = function (node, formula) {
+            node.formula = formula;
+            node.underEdit = false;
+            node.input = formula.unicode;
+        };
+        $scope.submit = function (node) {
             try {
                 node.error = {};
                 var newFormula = new WFF(node.input);
-                traverse($scope.treeData, function (n) {
-                    if (n.connectId == node.connectId) {
-                        n.formula = newFormula;
-                        n.underEdit = false;
-                        n.input = node.input;
-                    }
-                });
-
+                $scope.setFormula(node, newFormula);
+                if ('connectId' in node) {
+                    traverse($scope.treeData, function (n) {
+                        if (n.connectId == node.connectId) {
+                            $scope.setFormula(n, newFormula);
+                        }
+                    });
+                }
             } catch (ex) {
                 if (ex instanceof SyntaxError){
                     var msg = ex.message;
@@ -67,8 +71,33 @@ angular.module('ruzsa', ['sf.treeRepeat', 'ngMaterial', 'ngMessages'])
                 }
             }
         };
+        $scope.showEmptyNodeAlert = function () {
+            var alert = $mdDialog.alert({
+                title: 'This operation is not possible now',
+                textContent: 'Fill out empty sentence inputs first!',
+                ok: 'OK'
+            });
+            $mdDialog.show(alert);
+        };
+        $scope.checkForEmptyNodes = function () {
+            var emptyNodesPresent = false;
+            traverse($scope.treeData, function (node) {
+                if (!(node.formula)) {
+                    $scope.showEmptyNodeAlert();
+                    emptyNodesPresent = true;
+
+                    // Break traverse
+                    return true;
+                }
+            });
+            return emptyNodesPresent;
+        };
         $scope.greatestConnectId = 0;
-        $scope.addLeaves = function() {
+        $scope.addLeaves = function () {
+            var emptyNodesPresent = $scope.checkForEmptyNodes();
+            if (emptyNodesPresent) {
+                return;
+            }
             var id = ++$scope.greatestConnectId;
             var emptyNode = {formula: null,
                              editable: true,
@@ -78,14 +107,56 @@ angular.module('ruzsa', ['sf.treeRepeat', 'ngMaterial', 'ngMessages'])
             if (!$scope.treeData) {
                 $scope.treeData = emptyNode;
             } else {
-                traverse($scope.treeData, function(node) {
+                traverse($scope.treeData, function (node) {
                     if (!('children' in node) &&
-                        node.formula  // exclude newly added leaves
+                        // Exclude newly added leaves
+                        node.formula
                     ) {
-                        var emptyNodeClone = Object.assign({}, emptyNode);
+                        var emptyNodeClone = clone(emptyNode);
                         node.children = [emptyNodeClone];
                     }
                 });
             }
+        };
+        $scope.addCandidates = function (type, node) {
+            var emptyNodesPresent = $scope.checkForEmptyNodes();
+            if (emptyNodesPresent) {
+                return;
+            }
+            var candidate = {formula: null,
+                             editable: true,
+                             underEdit: true,
+                             input: '',
+                             candidate: true};
+            function makeDoubleCandidateClone() {
+                var doubleCandidateClone = clone(candidate);
+                doubleCandidateClone.children = [clone(candidate)];
+                return doubleCandidateClone;
+            }
+            traverse(node, function (n) {
+                if (!('children' in n) &&
+                    // Exclude newly added nodes
+                    n.formula
+                ) {
+                    if (type == 'or') {
+                        n.children = [
+                            clone(candidate),
+                            clone(candidate)
+                        ];
+                    } else if (type == 'and') {
+                        n.children = [
+                            makeDoubleCandidateClone()
+                        ];
+                    } else if (type == 'equi') {
+                        n.children = [
+                            makeDoubleCandidateClone(),
+                            makeDoubleCandidateClone()
+                        ];
+                    } else {
+                        throw new Error("Invalid type! " +
+                                        "Valid types are: 'or', 'and' and 'equi'.");
+                    }
+                }
+            });
         };
     });
---
+++ js/clone.js
@@ -0,0 +1,3 @@
+function clone(obj) {
+    return Object.assign({}, obj);
+}
