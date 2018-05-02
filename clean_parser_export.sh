661d2b5f03fb44c0aaf4e13b927ec1270bf3340e
Tamas Bitai <bitai.tamas@gmail.com>
2018-05-02 22:04:55 +0200

    Termux compatibility fixes; update mdi and material-design-icons; add package-lock.json

--- clean_parser_export.sh
+++ clean_parser_export.sh
@@ -4,7 +4,7 @@ FILE="js/lib/tarskiFOL.js"
 REGEXP="typeof exports"
 
 # Check if export part is in the expected format
-if [ `grep --count "$REGEXP" $FILE` == 0 ]; then
+if [ `grep -c "$REGEXP" $FILE` == 0 ]; then
     echo "ERROR! Couldn't find export part in parser file." 1>&2
     exit 1
 fi
